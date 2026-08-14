7f483a95f7359481a1772372c22da3fcc79511a3
Ray Lopez <raylopez509@gmail.com>
2026-08-14 15:30:08 -0700

    added logic to the checkboxes adding logic to the checkboxes makes it so i don't have to put that in the methods removed the logic in the methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
@@ -30,7 +27,6 @@ function App() {
     const newNumbers = target.checked ? [...player.round[currentRound].numbers, value] 
      : player.round[currentRound].numbers.filter((num) => num !== value);
 
-    console.log(newNumbers)
     const newPlayer = {
       ...player,
       round: {
@@ -42,7 +38,6 @@ function App() {
       }
     }
 
-    console.log(newPlayer);
     setPlayer(newPlayer);
 
   }
@@ -52,7 +47,6 @@ function App() {
     const id = target.id;
     const checked = target.checked;
     const newModifiers = {...player.round[currentRound].modifiers, [id]: checked}
-    console.log(newModifiers)
     const newPlayer = {
       ...player,
       round: {
@@ -63,7 +57,6 @@ function App() {
         } 
       }
     }
-    console.log(newPlayer);
     setPlayer(newPlayer);
   }
   
@@ -101,124 +94,116 @@ function App() {
 
   const nextRound = () => {
     let newRound = currentRound + 1
-
-    
-    let newPlayer = {...player, round: {
-      ...player.round,
-      [newRound]: {
-        numbers: [],
-        modifiers: {
-          plus2: false,
-          plus4: false,
-          plus6: false,
-          plus8: false,
-          plus10: false,
-          times2: false,
+    if(newRound > Object.keys(player.round).length) {
+      let newPlayer = {...player, round: {
+        ...player.round,
+        [newRound]: {
+          numbers: [],
+          modifiers: {
+            plus2: false,
+            plus4: false,
+            plus6: false,
+            plus8: false,
+            plus10: false,
+            times2: false,
+          }
         }
-      }
-    }}
-    setPlayer(newPlayer);
-
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(cb => {
-      cb.checked = false;
-    })
-
+      }}
+      setPlayer(newPlayer);     
+    }
     setCurrentRound(newRound);
-
   }
 
-  // const prevRound = () => {
-  //   let newRound = currentRound - 1;
-  // }
-
-
+  const prevRound = () => {
+    let newRound = currentRound - 1;
+    setCurrentRound(newRound);
+  }
   
   return (
     <>
       <div>Round {currentRound}</div>
       <div>ray</div>
       <label className='checkbox-card'>
-        <input type='checkbox' id='zero' value='0' onChange={handleChange}></input>
+        <input type='checkbox' id='zero' value='0' checked={player.round[currentRound].numbers.includes(0)} onChange={handleChange}></input>
         <span className='box'>0</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='one' value='1' onChange={handleChange}></input>
+        <input type='checkbox' id='one' value='1' checked={player.round[currentRound].numbers.includes(1)} onChange={handleChange}></input>
         <span className='box'>1</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='two' value='2' onChange={handleChange}></input>
+        <input type='checkbox' id='two' value='2' checked={player.round[currentRound].numbers.includes(2)} onChange={handleChange}></input>
         <span className='box'>2</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='three' value='3' onChange={handleChange}></input>
+        <input type='checkbox' id='three' value='3' checked={player.round[currentRound].numbers.includes(3)} onChange={handleChange}></input>
         <span className='box'>3</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='four' value='4' onChange={handleChange}></input>
+        <input type='checkbox' id='four' value='4' checked={player.round[currentRound].numbers.includes(4)} onChange={handleChange}></input>
         <span className='box'>4</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='five' value='5' onChange={handleChange}></input>
+        <input type='checkbox' id='five' value='5' checked={player.round[currentRound].numbers.includes(5)} onChange={handleChange}></input>
         <span className='box'>5</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='six' value='6' onChange={handleChange}></input>
+        <input type='checkbox' id='six' value='6' checked={player.round[currentRound].numbers.includes(6)} onChange={handleChange}></input>
         <span className='box'>6</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='seven' value='7' onChange={handleChange}></input>
+        <input type='checkbox' id='seven' value='7' checked={player.round[currentRound].numbers.includes(7)} onChange={handleChange}></input>
         <span className='box'>7</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='eight' value='8' onChange={handleChange}></input>
+        <input type='checkbox' id='eight' value='8' checked={player.round[currentRound].numbers.includes(8)} onChange={handleChange}></input>
         <span className='box'>8</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='nine' value='9' onChange={handleChange}></input>
+        <input type='checkbox' id='nine' value='9' checked={player.round[currentRound].numbers.includes(9)} onChange={handleChange}></input>
         <span className='box'>9</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='ten' value='10' onChange={handleChange}></input>
+        <input type='checkbox' id='ten' value='10' checked={player.round[currentRound].numbers.includes(10)} onChange={handleChange}></input>
         <span className='box'>10</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='eleven' value='11' onChange={handleChange}></input>
+        <input type='checkbox' id='eleven' value='11'checked={player.round[currentRound].numbers.includes(11)} onChange={handleChange}></input>
         <span className='box'>11</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='twelve' value='12' onChange={handleChange}></input>
+        <input type='checkbox' id='twelve' value='12' checked={player.round[currentRound].numbers.includes(12)} onChange={handleChange}></input>
         <span className='box'>12</span>
       </label>
 
       <label className='checkbox-card'>
-        <input type='checkbox' id='plus2' value='2' onChange={handleModifierChanges}></input>
+        <input type='checkbox' id='plus2' value='2' checked={player.round[currentRound].modifiers.plus2} onChange={handleModifierChanges}></input>
         <span className='box'>+2</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='plus4' value='4' onChange={handleModifierChanges}></input>
+        <input type='checkbox' id='plus4' value='4' checked={player.round[currentRound].modifiers.plus4} onChange={handleModifierChanges}></input>
         <span className='box'>+4</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='plus6' value='6' onChange={handleModifierChanges}></input>
+        <input type='checkbox' id='plus6' value='6' checked={player.round[currentRound].modifiers.plus6} onChange={handleModifierChanges}></input>
         <span className='box'>+6</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='plus8' value='8' onChange={handleModifierChanges}></input>
+        <input type='checkbox' id='plus8' value='8' checked={player.round[currentRound].modifiers.plus8} onChange={handleModifierChanges}></input>
         <span className='box'>+8</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='plus10' value='10' onChange={handleModifierChanges}></input>
+        <input type='checkbox' id='plus10' value='10' checked={player.round[currentRound].modifiers.plus10} onChange={handleModifierChanges}></input>
         <span className='box'>+10</span>
       </label>
       <label className='checkbox-card'>
-        <input type='checkbox' id='times2' onChange={handleModifierChanges}></input>
+        <input type='checkbox' id='times2' checked={player.round[currentRound].modifiers.times2} onChange={handleModifierChanges}></input>
         <span className='box'>x2</span>
       </label>
       <div>{getFinalScore()}</div>
       <div>{getRoundScore(currentRound)}</div>
 
-      <button>&lt;</button>
+      <button id='prevRound' onClick={prevRound} disabled={currentRound === 1}>&lt;</button>
       <button onClick={nextRound}>&gt;</button>
     </>
   )
