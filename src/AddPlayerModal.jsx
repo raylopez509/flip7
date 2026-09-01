4002380ddbd14c0d023c084ddff8071ec9780126
Ray Lopez <raylopez509@gmail.com>
2026-09-01 16:45:07 -0700

    changed input and button to a submit form element

--- src/AddPlayerModal.jsx
+++ src/AddPlayerModal.jsx
@@ -3,20 +3,23 @@ import { useRef } from "react";
 export default function AddPlayerModal({ onClose, handleAddNewPlayer }) {
  
   const playerNameRef = useRef(null);
-  const handleConfirm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const playerName = playerNameRef.current.value;
     handleAddNewPlayer(playerName);
     onClose();
   }
   return (
     <div>
-      <input
-        type="text"
-        ref={playerNameRef}
-        placeholder="Enter the player's name"
-      ></input>
-      <button onClick={handleConfirm}>Confirm</button>
-      <button onClick={onClose}>Cancel</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={playerNameRef}
+          placeholder="Enter the player's name"
+        ></input>
+        <button type="submit">Confirm</button>
+        <button onClick={onClose}>Cancel</button>
+      </form>
     </div>
   );
 }
