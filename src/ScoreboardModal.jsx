d34aee3dfe5e27b23434eb69c4abda557747bafd
Ray Lopez <raylopez509@gmail.com>
2026-09-02 16:59:50 -0700

    added table of all scores

--- src/ScoreboardModal.jsx
+++ src/ScoreboardModal.jsx
@@ -1,17 +1,26 @@
-export default function ScoreboardModal({ onClose, showScoreBoard }) {
-  const scores = Object.fromEntries(
-    Object.entries(showScoreBoard()).sort((a, b) => b[1] - a[1]),
-  );
-
+export default function ScoreboardModal({
+  onClose,
+  getRoundsArray,
+  getAllPlayerRoundScores,
+}) {
   return (
     <>
-      {Object.entries(scores).map(([name, score]) => (
-        <div key={name}>
-          <div>
-            {name}: {score}
-          </div>
-        </div>
-      ))}
+      <table>
+        <tr>
+          <th>Name</th>
+          {getRoundsArray().map((round) => (
+            <th>{round}</th>
+          ))}
+          <th>Total</th>
+        </tr>
+        {getAllPlayerRoundScores().map((player) => (
+          <tr>
+            {player.map((data) => (
+              <td>{data}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
       <button onClick={onClose}>Close</button>
     </>
   );
